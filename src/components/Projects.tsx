82863a286308daf05277305f6491e61dc3daf095
Luna <alicornlunaa@gmail.com>
2023-02-16 14:32:33 -0600

    Correctly published

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,25 +5,25 @@ function Projects(){
     return (
         <Grid container spacing={4} paddingLeft={8} paddingRight={8} paddingBottom={8} justifyContent="center">
             <Grid xs={4} item>
-                <Showcase title="Space Game" description={<p>A 2D survival orbit-based game.</p>} link="./SpaceGame" />
+                <Showcase title="Space Game" description={<p>A 2D survival orbit-based game.</p>} link="./#/SpaceGame" />
             </Grid>
             <Grid xs={4} item>
-                <Showcase title="Automating Minecraft" description={<p>Automating minecraft using algorithms</p>} link="./AutomatingMinecraft" />
+                <Showcase title="Automating Minecraft" description={<p>Automating minecraft using algorithms</p>} link="./#/AutomatingMinecraft" />
             </Grid>
             <Grid xs={4} item>
-                <Showcase title="Billiards" description={<p>Classic game of pool</p>} link="./Billiards" />
+                <Showcase title="Billiards" description={<p>Classic game of pool</p>} link="./#/Billiards" />
             </Grid>
             {/* <Grid xs={4} item>
-                <Showcase title="Grappling Hook Mod" description={<p>Lorem Ipsum</p>} link="./GrapplingHook" />
+                <Showcase title="Grappling Hook Mod" description={<p>Lorem Ipsum</p>} link="./#/GrapplingHook" />
             </Grid>
             <Grid xs={4} item>
-                <Showcase title="Boat Soccer Mod" description={<p>Lorem Ipsum</p>} link="./BoatSoccer" />
+                <Showcase title="Boat Soccer Mod" description={<p>Lorem Ipsum</p>} link="./#/BoatSoccer" />
             </Grid> */}
             <Grid xs={4} item>
-                <Showcase title="Pacman" description={<p>Classic game of Pac-Man</p>} link="./Pacman" />
+                <Showcase title="Pacman" description={<p>Classic game of Pac-Man</p>} link="./#/Pacman" />
             </Grid>
             {/* <Grid xs={4} item>
-                <Showcase title="Robot" description={<p>Lorem Ipsum</p>} link="./Robot" />
+                <Showcase title="Robot" description={<p>Lorem Ipsum</p>} link="./#/Robot" />
             </Grid> */}
         </Grid>
     );
