0fb5fea6d3237392bf7f3c2fa4f96865c624d88b
Luna <alicornlunaa@gmail.com>
2023-02-16 10:43:21 -0600

    Working on showcase

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { IconButton } from '@mui/material';
 import { Link } from 'react-router-dom';
+import Showcase from './Showcase';
 import "./Home.css";
 
 function Home(){
@@ -8,13 +9,20 @@ function Home(){
         <div className="content">
             <div className="header">
                 <div className="intro">
-                    <div className="name"><span className="big">Hi</span>, I'm Garrett Blankenship.</div>
+                    <div className="name">Hi, I'm Garrett Blankenship.</div>
                     <div className="small">I like to make stuff.</div>
                 </div>
                 <div className="links">
                     <Link to={"https://github.com/AlicornLunaa"}>
                         <GitHubIcon fontSize="large" sx={{ color: "white" }} />
                     </Link>
+                </div>
+            </div>
+            <div className="showcase">
+                <h2>Projects</h2>
+                <div className="showcaseprojects">
+                    <Showcase />
+                    <Showcase />
                 </div>
             </div>
         </div>
---
+++ src/components/Showcase.tsx
@@ -0,0 +1,16 @@
+import "./Showcase.css";
+
+function Showcase(){
+    return (
+        <div className="showcaseblock">
+            <div className="showcasetitle">
+                Test
+            </div>
+            <div className="showcasecontent">
+                Lorem Ipsum
+            </div>
+        </div>
+    );
+}
+
+export default Showcase;
