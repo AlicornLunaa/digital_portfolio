c4f5dfd060e1a4bc49102009ebbe5040ba5597fd
Luna <alicornlunaa@gmail.com>
2023-02-19 21:14:17 -0600

    Updating website

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,16 +1,43 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { Box } from '@mui/system';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import AboutMe from '../components/AboutMe';
 import Projects from '../components/Projects';
 import "./Home.css";
 
 function Home(){
+    const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
+    const [canvasCtx, setCanvasCtx] = useState<CanvasRenderingContext2D | null>(null);
+
+    const render = () => {
+        if(canvas == null) return;
+        if(canvasCtx == null) return;
+        
+        let parent = document.getElementsByClassName("header");
+        canvas.width = parent[0].clientWidth;
+        canvas.height = parent[0].clientHeight;
+
+        // canvasCtx.globalAlpha = 0;
+        // canvasCtx.fillStyle = "#FFAAAA";
+        // canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+        requestAnimationFrame(render);
+    }
+
+    useEffect(() => {
+        if(canvas == null) return;
+        setCanvasCtx(canvas.getContext("2d"));
+    }, [canvas]);
+
+    useEffect(() => render(), [canvasCtx]);
+
     return (
         <div className="content">
             <div className="header">
                 <div className="intro">Hi, I'm Garrett Blankenship.</div>
                 <div className="subheader">I like to make stuff.</div>
+                <canvas id="header-canvas" ref={(c) => setCanvas(c)} />
             </div>
             <div className="body">
                 <div className="section aboutme">
--- src/routes/Pacman.tsx
+++ src/routes/Pacman.tsx
@@ -7,7 +7,7 @@ function Pacman(){
             description="This was a project for my AP Computer Science A class. It is a simple Pac-man clone. It incorporates the A*
             pathfinding algorithm in order to give the location to the player for the ghosts."
         >
-            <video width="1280" height="720" controls>
+            <video controls>
                 <source src="./pacman/pacman.mp4" type="video/mp4" />
             </video>
         </ProjectPage>
